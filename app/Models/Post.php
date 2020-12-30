38065a7405021ff8d77acb7a6be1d0d5e792aed2
unknown <Administrator@DELSMB12DT108.ongc.ongcgroup.co.in>
2020-12-30 17:44:46 +0530

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        "title",
+        'content',
+        'slug',
+        'featured_image',
+        'is_featured',
+        'is_published'
+    ];
+
+
+    /**
+     * Get the user that owns the post.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    /**
+     * Get the category that owns the post.
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag');
+    }
+}
